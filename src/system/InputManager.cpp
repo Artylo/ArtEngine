d8d883f6e88752a0bdab96c641847dbad5c3ca5c
Arty <matglishev@gmail.com>
2024-05-06 13:39:12 +0300

    Basic mouse imput reimplemented.

--- src/system/InputManager.cpp
+++ src/system/InputManager.cpp
@@ -1,13 +1,38 @@
 #include "InputManager.h"
 #include <iostream>
 
+//This function refreshes the event pointer from SLD2 and sends it to all Game Managers. This gets processed every frame in Game.cpp.
 bool InputManager::ProcessEvent(const SDL_Event* event)
 {
 	event_ptr = event;
 
-	//@TODO: Maybe I can have the system work so I can have rebindable keys and it just references a bunch of preset actions.
-	//e.g. player_move_left = SDLK_LEFT | SDLK_W;
-	// And then every time I want some action to be done from some other object I find a way to send a request for player_move_left.
+	//Mouse
+	if (event->type == SDL_MOUSEMOTION)
+	{
+		//SDL_GetGlobalMouseState(&mouse_x, &mouse_y); //Mouse from 0,0 on desktop.
+		mouse_state = SDL_GetMouseState(&mouse_x, &mouse_y);
+	}
+	//Single button down event.
+	if (event->type == SDL_MOUSEBUTTONDOWN)
+	{
+		if (event->button.button == SDL_BUTTON_LEFT) std::cout << "LMB: " << mouse_x << "," << mouse_y << std::endl;
+		if (event->button.button == SDL_BUTTON_RIGHT) std::cout << "RMB: " << mouse_x << "," << mouse_y << std::endl;
+	}
+	//Single releases.
+	if (event->type == SDL_MOUSEBUTTONUP)
+	{
+		if (event->button.button == SDL_BUTTON_LEFT) std::cout << "-LMB: " << mouse_x << "," << mouse_y << std::endl;
+		if (event->button.button == SDL_BUTTON_RIGHT) std::cout << "-RMB: " << mouse_x << "," << mouse_y << std::endl;
+	}
+	//Continuous mouse button updates (every frame).
+	//if (mouse_state & SDL_BUTTON(SDL_BUTTON_Left)) std::cout << "LMB: " << mouse_x << "," << mouse_y << std::endl;
+	
+	//Mouse Wheel
+	if (event->type == SDL_MOUSEWHEEL)
+	{
+		if(event->wheel.y > 0) std::cout << "Mouse wheel up." << std::endl;
+		else std::cout << "Mouse wheel down." << std::endl;
+	}
 
 	switch (event->type)
 	{
--- src/system/InputManager.h
+++ src/system/InputManager.h
@@ -6,6 +6,13 @@ class InputManager
 {
 
 public:
+
+	//Mouse
+	Uint32 mouse_state = NULL;
+	int mouse_x = NULL;
+	int mouse_y = NULL;
+
+	//System
 	const SDL_Event* event_ptr;
 	bool ProcessEvent(const SDL_Event *event);
 };
